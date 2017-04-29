44f14b9e48e318c988c0de8d535c69bdff282ae8
Niall O'Callaghan <noccer@gmail.com>
2017-04-29 18:12:21 +1000

    First React component.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 $ = jQuery = require('jquery'); // nice little trick to say that both $ and jQuery resolve to require('jquery'). We need to require this because boostrap expects jQuery to be in the global namespace.
+var React = require('react');
+var Home = require('./components/homepage.js');
 
-// var App will not be global code, it will be in its own module.
-var App = console.log("Hello world from Browserify.");
-
-// Browserify is a bundler.
-// Browserify uses the commonJS pattern which uses module.exports to define what the file exports.
-// it will go through each file and wrap them up in their own context.
-
-module.exports = App;
+React.render(<Home/>, document.getElementById('app'));
