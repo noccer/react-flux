82cf501701edc9f60c37d06e0434487d60418a63
Niall O'Callaghan <noccer@gmail.com>
2017-05-03 08:53:13 +1000

    Controlled Components.

--- src/components/authors/manageAuthorPage.jsx
+++ src/components/authors/manageAuthorPage.jsx
@@ -4,9 +4,30 @@ var React = require('react');
 var AuthorForm = require('./authorForm.jsx');
 
 var ManageAuthorPage = React.createClass({
+    // top-level components are the best place to manage state
+    getInitialState: function() {
+        return {
+            author: {
+                id: "",
+                firstName: "",
+                lastName: ""
+            }
+        };
+    },
+
+    setAuthorState: function(event) { // this funtion will be called for every keypress that we make
+        var field = event.target.name;
+        var value = event.target.value;
+        console.log(value);
+        this.state.author[field] = value;
+        return this.setState({
+            author: this.state.author
+        });
+    },
+
     render: function() {
         return (
-            <AuthorForm />
+            <AuthorForm author={this.state.author} onChange={this.setAuthorState}/>
         );
     }
 });
